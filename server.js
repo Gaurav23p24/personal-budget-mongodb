091e89106a26f6e48efa2209973d1184530deb96
Gaurav23p24 <gpatel23@charlotte.edu>
2024-09-15 15:33:02 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,37 +1,44 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title:'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Groceries',
-        budget: 110
-    },
-    {
-        title: 'Personal',
-        budget: 50
-    },
-    {
-        title: 'Loan',
-        budget: 500
-    },
-    {
-        title: 'Pets',
-        budget: 40
-    },
-    ]  
-};
+const budget = require("./budget.json");
+
+app.use(cors());
+
+// const budget = {
+//     myBudget: [
+//     {
+//         title:'Eat out',
+//         budget: 25
+//     },
+//     {
+//         title: 'Rent',
+//         budget: 375
+//     },
+//     {
+//         title: 'Groceries',
+//         budget: 110
+//     },
+//     {
+//         title: 'Personal',
+//         budget: 50
+//     },
+//     {
+//         title: 'Loan',
+//         budget: 500
+//     },
+//     {
+//         title: 'Pets',
+//         budget: 40
+//     },
+//     ]  
+// };
+
+
 
 app.get('/hello', (req, res)=> {
     res.send('Hello World from Gaurav Bharatkumar Patel');
